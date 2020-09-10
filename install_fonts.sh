71f954d4fb890506272879c4bc3e2b902c061540
BIT-zhwang <46585564+BIT-zhwang@users.noreply.github.com>
2020-09-10 11:37:09 +0800

    add fonts

---
+++ install_fonts.sh
@@ -0,0 +1,29 @@
+sudo mkdir -p /usr/share/fonts/custom
+
+sudo mv Monaco.ttf /usr/share/fonts/custom
+sudo chmod 744 /usr/share/fonts/custom/Monaco.ttf
+
+sudo mv consola.ttf /usr/share/fonts/custom
+sudo chmod 744 /usr/share/fonts/custom/consola.ttf
+
+sudo mv consolab.ttf /usr/share/fonts/custom
+sudo chmod 744 /usr/share/fonts/custom/consolab.ttf
+
+sudo mv consolai.ttf /usr/share/fonts/custom
+sudo chmod 744 /usr/share/fonts/custom/consolai.ttf
+
+sudo mv consolaz.ttf /usr/share/fonts/custom
+sudo chmod 744 /usr/share/fonts/custom/consolaz.ttf
+
+sudo mv msyh.ttc /usr/share/fonts/custom
+sudo chmod 744 /usr/share/fonts/custom/msyh.ttc
+
+sudo mv msyhbd.ttc /usr/share/fonts/custom
+sudo chmod 744 /usr/share/fonts/custom/msyhbd.ttc
+
+sudo mv msyhl.ttc /usr/share/fonts/custom
+sudo chmod 744 /usr/share/fonts/custom/msyhl.ttc
+
+#sudo mkfontscale  #生成核心字体信息
+#sudo mkfontdir
+#sudo fc-cache -fv 
