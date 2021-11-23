70e2af6007234e87aae85a9dcccb305a8d950c80
戶下之羽 <33632784+Chernfalin@users.noreply.github.com>
2021-11-23 11:27:28 +0800

    Update install_fonts.sh

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,29 +1,32 @@
+mkdir fonts
+find Fonts/ -type f -print0 | xargs -0 cp -t fonts/
+
 sudo mkdir -p /usr/share/fonts/custom
 
-sudo mv Monaco.ttf /usr/share/fonts/custom
+sudo mv fonts/Monaco.ttf /usr/share/fonts/custom
 sudo chmod 744 /usr/share/fonts/custom/Monaco.ttf
 
-sudo mv consola.ttf /usr/share/fonts/custom
+sudo mv fonts/consola.ttf /usr/share/fonts/custom
 sudo chmod 744 /usr/share/fonts/custom/consola.ttf
 
-sudo mv consolab.ttf /usr/share/fonts/custom
+sudo mv fonts/consolab.ttf /usr/share/fonts/custom
 sudo chmod 744 /usr/share/fonts/custom/consolab.ttf
 
-sudo mv consolai.ttf /usr/share/fonts/custom
+sudo mv fonts/consolai.ttf /usr/share/fonts/custom
 sudo chmod 744 /usr/share/fonts/custom/consolai.ttf
 
-sudo mv consolaz.ttf /usr/share/fonts/custom
+sudo mv fonts/consolaz.ttf /usr/share/fonts/custom
 sudo chmod 744 /usr/share/fonts/custom/consolaz.ttf
 
-sudo mv msyh.ttc /usr/share/fonts/custom
+sudo mv fonts/msyh.ttc /usr/share/fonts/custom
 sudo chmod 744 /usr/share/fonts/custom/msyh.ttc
 
-sudo mv msyhbd.ttc /usr/share/fonts/custom
+sudo mv fonts/msyhbd.ttc /usr/share/fonts/custom
 sudo chmod 744 /usr/share/fonts/custom/msyhbd.ttc
 
-sudo mv msyhl.ttc /usr/share/fonts/custom
+sudo mv fonts/msyhl.ttc /usr/share/fonts/custom
 sudo chmod 744 /usr/share/fonts/custom/msyhl.ttc
 
-#sudo mkfontscale  #生成核心字体信息
-#sudo mkfontdir
-#sudo fc-cache -fv 
+sudo mkfontscale  #生成核心字体信息
+sudo mkfontdir
+sudo fc-cache -fv 
